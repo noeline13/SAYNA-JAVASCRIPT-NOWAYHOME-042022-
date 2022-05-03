19942e0421ef8df71b564df40a9c9f8fbb077d1e
Noeline <noeline.ravoniarisoa7@gmail.com>
2022-05-03 11:53:21 +0300

    button2

---
+++ scripts.js
@@ -0,0 +1,49 @@
+// effet sur le button 2
+let button2 = document.querySelector(".button2");
+button2.addEventListener("mouseover", myFunction);
+
+function myFunction() {
+    let css = button2.style;
+    css.fontWeight = "800";
+    css.backgroundColor = "white";
+    css.color = "red";
+    css.boxShadow = " inset 0 0 15em 0 white ,0 0 0.5em 0 white";
+    css.textShadow = "inset 0 0 15em 0 red , 0 0 0.5em 0 red";
+};
+button2.addEventListener("mouseout", myFunctions);
+
+function myFunctions() {
+    let css = button2.style;
+    css.fontWeight = "400";
+    css.backgroundColor = "transparent";
+    css.color = "white";
+    css.boxShadow = "none";
+    css.textShadow = "none";
+}
+
+//effet sur le button 1
+let button1 = [...document.querySelectorAll(".button1")];
+button1.forEach(btn => {
+    btn.addEventListener("mouseover", myFunctio)
+});
+
+function myFunctio() {
+    let css = button1.style;
+    css.fontWeight = "800";
+    css.backgroundColor = "white";
+    css.color = "red";
+    css.boxShadow = " inset 0 0 15em 0 white ,0 0 0.5em 0 white";
+    css.textShadow = "inset 0 0 15em 0 red , 0 0 0.5em 0 red";
+};
+button1.forEach(btn => {
+    btn.addEventListener("mouseout", myFunctios)
+});
+
+function myFunctios() {
+    let css = button1.style;
+    css.fontWeight = "400";
+    css.backgroundColor = "white";
+    css.color = "red";
+    css.boxShadow = "none";
+    css.textShadow = "none";
+};
