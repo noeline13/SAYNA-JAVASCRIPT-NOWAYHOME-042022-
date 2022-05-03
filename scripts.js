43c72c0f982e18235818a1cec60a0781d1fb8804
Noeline <noeline.ravoniarisoa7@gmail.com>
2022-05-03 16:28:32 +0300

    ajout des image manquantes

--- scripts.js
+++ scripts.js
@@ -30,10 +30,10 @@ button1.forEach(btn => {
 function myFunctio(btn) {
     let css = btn.style;
     css.fontWeight = "800";
-    css.backgroundColor = "white";
-    css.color = "red";
-    css.boxShadow = " inset 0 0 15em 0 white ,0 0 0.5em 0 white";
-    css.textShadow = "inset 0 0 15em 0 red , 0 0 0.5em 0 red";
+    css.backgroundColor = "red";
+    css.color = "white";
+    css.boxShadow = " inset 0 0 15em 0 red ,0 0 0.5em 0 red";
+    css.textShadow = "inset 0 0 15em 0 white , 0 0 0.5em 0 white";
 };
 button1.forEach(btn => {
     btn.addEventListener("mouseout", () => { myFunctios(btn) })
@@ -48,4 +48,55 @@ function myFunctios(btn) {
     css.textShadow = "none";
 };
 
-//footer
+//souligner le menu
+let menu = document.querySelectorAll("li")
+for (let i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("mouseover", () => {
+        menu[i].style.textDecoration = "line-through"
+    })
+    menu[i].addEventListener("mouseout", () => {
+        menu[i].style.textDecoration = "none"
+    })
+}
+
+//fade in titre
+let title = document.querySelectorAll("h1")
+for (let i = 0; i < title.length; i++) {
+    title[i].classList.add("slider")
+}
+
+//fade in paragraphe
+let paragraphe = document.querySelectorAll("p")
+for (let i = 0; i < paragraphe.length; i++) {
+    paragraphe[i].classList.add("slide")
+}
+// fade in sur le logo
+let logo = document.querySelector("#marvel")
+logo.classList.add("move");
+
+//scroll de l'image
+let image = document.querySelector("")
+image.addEventListener("onclick", scrollWin())
+
+function scrollWin() {
+    window.scrollTo(0, 500);
+}
+
+
+/*changement de couleur du  social media dans le footer*/
+let footer = document.querySelectorAll(".image")
+footer.forEach(img => {
+    img.addEventListener("mouseover", () => { move(img) })
+});
+
+function move(img) {
+    img.classList.add("hover")
+};
+
+footer.forEach(img => {
+    img.addEventListener("mouseout", () => { moves(img) })
+});
+
+function moves(img) {
+    img.classList.remove("hover")
+};
